cff8c6fff53c59c15bd2834fcd8fe5d8d2d56244
void67913 <ohiedits0@gmail.com>
2026-09-20 00:53:01 +0600

    Update model name from 'gpt-5.6-mini' to 'gpt-5.6-luna'

--- src/commands/Search/modules/search_ai.js
+++ src/commands/Search/modules/search_ai.js
@@ -12,7 +12,7 @@ export default {
 
         try {
             const response = await openai.responses.create({
-                model: 'gpt-5.6-mini',
+                model:'gpt-5.6-luna',
                 input: query
             });
 
